a25816578c20b434232fa35f6151392e2c723ad0
shakhawat-khan <shakha0watt@gmail.com>
2025-06-17 18:29:36 +0600

    first commit

---
+++ __brick__/lib/module/{{name.snakeCase()}}/api/{{name.snakeCase()}}_api.dart
@@ -0,0 +1,8 @@
+class {{name.pascalCase()}}Api {
+  const {{name.pascalCase()}}Api();
+  // Add your API methods here
+  // Example:
+  // Future<List<{{name.pascalCase()}}Model>> fetch{{name.pascalCase()}}List() async {
+  //   // Your API call logic
+  // }
+}
